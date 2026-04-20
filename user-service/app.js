f7fc1183a78a336fb559c10b4f58a6841efaef38
Pramay11 <pramaygawali@gmail.com>
2026-04-20 16:20:06 +0530

    Refactor user service to remove MongoDB and add login
    Removed MongoDB integration and user management routes. Added a health check endpoint and a login endpoint.

--- user-service/app.js
+++ user-service/app.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
-
-const User = mongoose.model('User', {
-  name: String,
-  score: Number
+app.get('/health', (req, res) => {
+  res.send("User Service Running");
 });
 
-app.post('/user', async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  res.send(user);
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  res.json({ message: `Welcome ${username}` });
 });
 
-app.get('/users', async (req, res) => {
-  const users = await User.find();
-  res.send(users);
-});
-
-app.listen(3000, () => console.log('User service running'));
+app.listen(3000, () => console.log("User Service on 3000"));
